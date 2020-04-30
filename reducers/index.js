161665f8e84fbb5259e4333574621dda47416dec
Pawel Bala <pawel3ala@gmail.com>
2020-04-29 21:24:34 -0500

    allign code to redux-persist v6

--- reducers/index.js
+++ reducers/index.js
@@ -1,9 +1,7 @@
-import { combineReducers } from 'redux'
 import auth from './auth_reducer'
 import jobs from './jobs_reducer'
 import likedJobs from './likes_reducer'
 
-
-export default combineReducers({
+export default ({
     auth, jobs, likedJobs
 })
