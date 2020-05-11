0fd064f41db00b70cc6150f95d31a6732ca76682
Pawel Bala <pawel3ala@gmail.com>
2020-05-11 13:33:28 -0500

    MainNavigator tab styling

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,19 +1,42 @@
+/* eslint-disable react/display-name */
 
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapScreen from '../screens/MapScreen'
 import DeckScreen from '../screens/DeckScreen'
 import ReviewNavigator from '../navigation/ReviewNavigator'
+import { Icon } from 'react-native-elements'
 
 const Tab = createBottomTabNavigator()
 
-const MainNavigator = () => {
+const navigatorAllScreensOptions = ({ route }) => ({
+
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'map') iconName = 'map'
+    else if (route.name === 'deck') iconName = 'work'
+    else if (route.name === 'review') iconName = 'bookmark'
+
     return (
-        <Tab.Navigator>
-        <Tab.Screen name="map" component={MapScreen} />
-        <Tab.Screen name="deck" component={DeckScreen} />
-        <Tab.Screen name="rewiew" component={ReviewNavigator} />
-      </Tab.Navigator>
+      <Icon
+        name={iconName}
+        color={color}
+        size={30}
+      />
     )
+  }
+})
+
+const MainNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={navigatorAllScreensOptions}
+    >
+      <Tab.Screen name="map" component={MapScreen} />
+      <Tab.Screen name="deck" component={DeckScreen} />
+      <Tab.Screen name="review" component={ReviewNavigator} />
+    </Tab.Navigator>
+  )
 }
 export default MainNavigator
